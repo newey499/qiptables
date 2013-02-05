bb786dba8739767abfc9017b2f03fc285dbc926a
Chris Newey <newey499@hotmail.com>
2013-02-05 04:44:34 +0000

    Add code to preserve main window size and position

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
+
+
+MainWindow::MainWindow(QString organization, QString application, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    this->organization = organization;
+    this->application = application;
+
     ui->setupUi(this);
+    QSettings settings(organization, application);
+    restoreGeometry(settings.value("geometry").toByteArray());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+
+    int ret = QMessageBox::warning(this, tr("My Application"),
+                                   tr("Exit the Application?"),
+                                   QMessageBox::Yes | QMessageBox::No,
+                                   QMessageBox::Yes);
+    if (ret == QMessageBox::Yes)
+    {
+        QSettings settings(organization,application);
+        settings.setValue("geometry", saveGeometry());
+        event->accept();
+    }
+    else
+    {
+        event->ignore();
+    }
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QCloseEvent>
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QSettings>
 
 namespace Ui {
 class MainWindow;
@@ -10,11 +13,20 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
-    explicit MainWindow(QWidget *parent = 0);
+
+    explicit MainWindow(QString organization = "git:bitbucket.org:newey499.qiptables.git",
+                        QString application  = "qiptables",
+                        QWidget *parent = 0);
     ~MainWindow();
-    
+
+    QString organization;
+    QString application;
+
+protected:
+    virtual void closeEvent(QCloseEvent *event);
+
 private:
     Ui::MainWindow *ui;
 };
